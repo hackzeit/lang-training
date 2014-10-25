a4bdd91f2f2a5318d7e02f92ea17260c350bcd5d
Frank Moreno <frankmoreno1993@gmail.com>
2014-10-25 01:16:31 -0500

    C++: ejemplos de for documentados en controlflow.slide

--- cpp/controlflow/for.cpp
+++ cpp/controlflow/for.cpp
@@ -1,25 +1,41 @@
-void for1(){ // OMIT
-  int f = 1;
-  for( int i = 2; i <= 10; i++)
-    f *= i;
-  std::cout << "factorial: 10! = " << f << std::endl;
+#include <iostream>
+
+void for1() { // OMIT
+  for (int i = 10; i > 0; i++) {
+    std::cout << i << ", ";
+    if (i == 4) break;
+  }
+  // Salida: 10, 9, 8, 7, 6, 5, 4,
 } // OMIT
 
-void for2(){ // OMIT
-  string str {"Hola!"};
-  for (char c : str) // for (auto c : str)
-  {
-    std::cout << "[" << c << "]";
+void for2() { // OMIT
+  for (int i = 10; i > 0; i++) {
+    if (i == 4) continue;
+    std::cout << i << ", ";
   }
+  // Salida: 10, 9, 8, 7, 6, 5, 3, 2, 1,
+} // OMIT
+
+void for3(){ // OMIT
+  charg str[] = "Hola!"; // ['H','o','l','a','!',\0]
+  for (int i = 0; i < 6; i++) 
+    std::cout << "[" << str[i] << "]";
   std::cout << '\n';
-} //OMI
+  // Salida: [H][o][l][a][!][]
+} // OMIT
 
-
-void for3(){ //OMIT
+void for4(){ //OMIT
   charg str[] = "Hola!"; // ['H','o','l','a','!',\0]
   for (char c : str) 
-  {
     std::cout << "[" << c << "]";
-  }
   std::cout << '\n';
+  // Salida: [H][o][l][a][!][]
 } // OMIT
+
+void for5(){ // OMIT
+  string str {"Hola!"};
+  for (char c : str) // for (auto c : str)
+    std::cout << "[" << c << "]";
+  std::cout << '\n';
+  // Salida: [H][o][l][a][!]
+} //OMIT
