b0dde1b7fde44e8b5efbfd39ec3395fa943cc61d
Frank Moreno <frankmoreno1993@gmail.com>
2014-10-26 19:50:57 -0500

    c++: compilacion - ejemplo de biblioteca aritmetica listo

---
+++ cpp/compilation/aritmetica.cpp
@@ -0,0 +1,15 @@
+int suma(int a, int b) {
+  return a + b;
+}
+
+int resta(int a, int b) {
+  return a - b;
+}
+
+int mul(int a, int b) {
+  return a * b;
+}
+
+int div(int a, int b) {
+  return a / b;
+}
---
+++ cpp/compilation/aritmetica.h
@@ -0,0 +1,5 @@
+// aritmetica.h (h de header)
+int suma(int, int);
+int resta(int, int);
+int mul(intm int);
+int div(int, int);
