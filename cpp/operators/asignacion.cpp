b16b337ff8213e456edfc595126eab9ebc457cac
Frank Moreno <frankmoreno1993@gmail.com>
2014-10-27 23:41:57 -0500

    c++ operators/asignacion.cpp - primer ejemplo

---
+++ cpp/operators/asignacion.cpp
@@ -0,0 +1,8 @@
+void asignacion1() {
+  int a,b;   // a:?,  b:?
+  a = 10;    // a:10, b:?
+  b = 4;     // a:10, b:4
+  a = b;     // a:4,  b:4
+  b = 7;     // a:4,  b:7
+  printf("a:%d\tb:%d\n",a,b);
+}
