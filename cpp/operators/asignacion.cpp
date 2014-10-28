045be318aaf323eab6a845be7d4b2b2696ce13ed
Frank Moreno <frankmoreno1993@gmail.com>
2014-10-27 23:45:07 -0500

    c++ operators/asignacion.cpp - segundo ejemplo

--- cpp/operators/asignacion.cpp
+++ cpp/operators/asignacion.cpp
@@ -1,8 +1,19 @@
-void asignacion1() {
+void asignacion1() { // OMIT
   int a,b;   // a:?,  b:?
   a = 10;    // a:10, b:?
   b = 4;     // a:10, b:4
   a = b;     // a:4,  b:4
   b = 7;     // a:4,  b:7
   printf("a:%d\tb:%d\n",a,b);
-}
+} // OMIT
+
+void asignacion2() { // OMIT
+  int m,n,p;    // m:?, n:?, p:?
+  // p = 5;     // m:?, n:?, p:5
+  // n = p;     // m:?, n:5, p:5
+  // m = n;     // m:5, n:5, p:5
+  m = n = p = 5;
+} // OMIT
+
+void asignacion3() { // OMIT
+} // OMIT
