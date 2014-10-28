7ca70b664832af5e3d167b7a67c562463e0831ad
Frank Moreno <frankmoreno1993@gmail.com>
2014-10-27 23:52:38 -0500

    c++ operators/aritmeticos.cpp - para enteros

---
+++ cpp/operators/aritmeticos.cpp
@@ -0,0 +1,10 @@
+void aritmeticos_int() { // OMIT
+  a = 2 + 4;     // adicion         =>  a:6
+  b = 8 - 3;     // sustraccion     =>  b:5
+  c = 5 * 6;     // multiplicacion  =>  c:30
+  d = 10 / 3;    // division        =>  d:3
+  e = 10 % 3;    // modulo          =>  e:1
+} // OMIT
+
+void aritmeticos_float() { // OMIT
+} // OMIT
