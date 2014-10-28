0fccb1186cc91664654e529bf22f308ac2df120b
Frank Moreno <frankmoreno1993@gmail.com>
2014-10-27 23:59:03 -0500

    c++ operators/inc_dec.cpp - para decrementos

--- cpp/operators/inc_dec.cpp
+++ cpp/operators/inc_dec.cpp
@@ -3,10 +3,14 @@
 void incremento() { // OMIT
   int x,y,z;
   x = 3;
-  y = x++;
-  z = ++x;
+  y = x++;  // x:4, y:3
+  z = ++x;  // x:5, z:5
   printf("x:%d\ty:%d\tz:%d\n",x,y,z);
 } // OMIT
 
 void decremento() { // OMIT
+  int x,y,z;
+  x = 3;
+  y = --x;  // x:2, y:2
+  z = x--;  // x:1, z:2
 } // OMIT
