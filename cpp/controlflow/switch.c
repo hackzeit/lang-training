f95fc7dc6410e345f78c6d29d385bdd8c366470f
Frank Moreno <frankmoreno1993@gmail.com>
2014-11-02 11:49:25 -0500

    conversión de controlflow/{switch.cpp -> switch.c}

--- cpp/controlflow/switch.c
+++ cpp/controlflow/switch.c
@@ -1,14 +1,14 @@
-#include <iostream>
+#include <stdio.h>
 
 void switch1()
 { // START 1 OMIT
   int x = 2;
   switch(x) {
     case 1:
-      std::cout << "valor de x == 1";
+      puts( "valor de x == 1");
       break;
     case 2:
-      std::cout << "valor de x == 2"; 
+      puts("valor de x == 2"); 
   }
 } // STOP 1 OMIT
 
@@ -17,13 +17,13 @@ void switch2()
   int x = 6;
   switch(x) {
     case 1:
-      std::cout << "valor de x == 1";
+      puts("valor de x == 1");
       break;
     case 2:
-      std::cout << "valor de x == 2"; 
+      puts("valor de x == 2"); 
       break;
     default:
-      std::cout << "valor de x != 1 y x != 2";
+      puts("valor de x != 1 y x != 2");
       break;
   }
 } // STOP 2 OMIT
@@ -33,17 +33,17 @@ void switch3()
   int x = 3;
   switch(x) {
     case 6:
-      std::cout << "x = 6\n";
+      puts("x = 6");
       break;
     case 5:
-      std::cout << "x == 5\n";
+      puts("x == 5");
     case 4:
-      std::cout << "x >= 4\n";
+      puts("x >= 4");
     case 3:
-      std::cout << "x >= 3\n";
+      puts("x >= 3");
       break;
     default:
-      std::cout << "x < 3 o x > 6\n";
+      puts("x < 3 o x > 6");
       break; // El ultimo 'break;' es innecesario
   }
 } // STOP 3 OMIT
@@ -53,24 +53,24 @@ void switch4()
   int x = 3;
   switch(x) {
     case 6:
-      std::cout << "x = 6\n";
+      puts("x = 6");
       break;
     case 5:
     case 4:
     case 3:
-      std::cout << "x es 3, 4 o 5\n";
+      puts("x es 3, 4 o 5");
       break;
     default:
-      std::cout << "x < 3 o x > 6\n";
+      puts("x < 3 o x > 6");
   }
 } // STOP 4 OMIT
 
 // START main OMIT
 int main()
 {
-  switch1(); std::cout << std::endl;
-  switch2(); std::cout << std::endl;
-  switch3(); std::cout << std::endl;
-  switch4(); std::cout << std::endl;
+  switch1();
+  switch2();
+  switch3();
+  switch4(); puts("");
 }
 // STOP main OMIT
