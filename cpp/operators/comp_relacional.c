898546a897ab1517a290ac0b1ecfd250c9cdd84b
Frank Moreno <frankmoreno1993@gmail.com>
2014-11-02 11:26:05 -0500

    C++: operators/comp_relacional.c listo

--- cpp/operators/comp_relacional.c
+++ cpp/operators/comp_relacional.c
@@ -1,22 +1,28 @@
+#include <stdio.h>
+
 void comp() { // OMIT
-  bool v;
   int a = 2, b = 3, c = 6;
-  v = (7 == 5);     // v:false
-  v = (b != 2);     // v:true
-  V = (a == 5)      // v:false
-  V = ((b=2) == a)  // v:true
+  printf("%d\t", 7 == 5);      // false
+  printf("%d\t", b != 2);      // true
+  printf("%d\t", a == 5);      // false
+  printf("%d\n", (b=2) == a);  // true
+  printf("a:%d\tb:%d\tc:%d\n",a,b,c);
 } // OMIT
 
 void relacional() { // OMIT
   bool v;
   int a = 2, b = 3, c = 6;
-  v = (5 > 4);      // v:true
-  v = (6 >= 6);     // v:true
-  v = (5 < 5);      // v:false
-  v = (a*b >= c);   // v:true, pues (2*3 >= 6)
-  v = (b+4 > a*c);  // v:false, pues (3+4 > 2*6)
+  printf("%d\t", 5 > 4);      // true
+  printf("%d\t", 6 >= 6);     // true
+  printf("%d\t", 5 < 5);      // false
+  printf("%d\t", a*b >= c);   // true, pues (2*3 >= 6)
+  printf("%d\n", b+4 > a*c);  // false, pues (3+4 > 2*6)
+  printf("a:%d\tb:%d\tc:%d\n",a,b,c);
 } // OMIT
 
 int main() {
+  comp();
+  printf("\n");
+  relacional();
   return 0;
 }
