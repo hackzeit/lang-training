b253ac4947765acd622363254a328c1d11987cf7
Frank Moreno <frankmoreno1993@gmail.com>
2014-11-03 01:18:15 -0500

    C++: arguments/arguments.c escribiendo mejor los hitos para mostrar código en los slides

--- cpp/arguments/arguments.c
+++ cpp/arguments/arguments.c
@@ -2,47 +2,61 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv){
+  // START var necesaria OMIT
   int A[8];
+  int v;
+  // STOP var necesaria OMIT
   
+  // START minarg OMIT
   if (argc < 4 ) {
     puts("faltan argumenos. Deden ser por lo menos 4");
     return 1;
   }
+  // STOP minarg OMIT
   
-  int n = argc < 10 ? argc -2: 8;
+  int n = argc < 10 ? argc -2: 8; // numero de elementos llenos en A
+
   for (int i = 0; i < n; i++) {
     A[i] = atoi(argv[i+2]);
   }
 
-  int s = 0;
   switch ( argv[1][0] ) {
-    case '+':
+    case 's':
+      // START SUM OMIT
+      v = 0;
       for (int i = 0; i < n; i++)
-        s += A[i];
+        v += A[i];
+      // STOP SUM OMIT
       break;
-    case '-':
+    case 'r':
+      // START RES OMIT
       A[0] = atoi(argv[2]);
       A[1] = atoi(argv[3]);
-      s = A[0] - A[1];
+      v = A[0] - A[1];
+      // STOP RES OMIT 
       break;
-    case 'x':
-      s = 1;
+    case 'm':
+      // START MUL OMIT
+      v = 1;
       for (int i = 0; i < n; i++)
-        s *= A[i];
-        break;
-    case '/':
+        v *= A[i];
+      // STOP MUL OMMIT
+      break;
+    case 'd':
+      // START DIV OMIT
       A[0] = atoi(argv[2]);
       A[1] = atoi(argv[3]);
       if (A[1] == 0) {
         puts("division entre cero invalida");
         return 3;
       }
-      s = A[0] / A[1];
+      v = A[0] / A[1];
+      // STOP DIV OMIT
       break;
     default:
       puts("operador no reconocido");
       return 2;
   } 
-  printf("%d\n",s);
+  printf("%d\n",v);
   return 0;
 }
