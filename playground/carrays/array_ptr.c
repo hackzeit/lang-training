44b8ba3c9e1210b8ac3fe82ceea04abf47b31acd
Frank Moreno <frankmoreno1993@gmail.com>
2014-11-09 23:00:07 -0500

    Playground: ejemplo de arrays

--- playground/carrays/array_ptr.c
+++ playground/carrays/array_ptr.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
      
 int main(void) {
-  int A[] = {1,2,3,3,4,5,6,7};
+  int A[] = {1,2,3,4,5,6,7};
   printf("%d\n", A[5]);
   printf("%d\n", 5[A]);
   printf("%d\n", *(5+A));
   return 0;
-}    
+}
