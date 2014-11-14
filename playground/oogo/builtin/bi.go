6a14ef6bdc7c258d4843aa345ee898b11ee3d4b9
Frank Moreno <frankmoreno1993@gmail.com>
2014-11-14 13:54:41 -0500

    Playgroung: Añadiendo comentarios a oogo/bultin/bi.go

--- playground/oogo/builtin/bi.go
+++ playground/oogo/builtin/bi.go
@@ -2,8 +2,12 @@ package main
 
 import "fmt"
 
+// Es necesario hacer esto porque no se puede aplicar métodos
+// con tipos de datos no escrito en el mismo archivo. En este
+// caso int es un built-in types
 type Int int
 
+// Ejemplo de OOP con built-in type
 func (a *Int) duplicar(){
   *a *= 2
 }
